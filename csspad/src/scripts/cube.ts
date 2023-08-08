a8181795708a8c421f32f70820f002f108fed9a1
Slawomir Rodak <slawomir.rodak@schibsted.com>
2023-08-08 11:21:01 +0200

    feat: Introduce CUBE demo

---
+++ csspad/src/scripts/cube.ts
@@ -0,0 +1,8 @@
+import './../styles/cube.scss'
+// import { $ } from './dom'
+
+
+// const $app = $('#app')!
+// $app.innerHTML = `Everything is ok in CUBE`;
+
+console.log('Everything is ok in CUBE');
